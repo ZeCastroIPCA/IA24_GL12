d056da81b0ac45607e687007d90233035f98e559
Zé Castro <a20351@alunos.ipca.pt>
2024-11-11 18:11:12 +0000

    added jobs variable that has all the info necessary

--- csp.py
+++ csp.py
@@ -4,7 +4,7 @@ from constraint import *
 from parse_file import parse_file
 
 # Read the file
-file_path = './dataset.txt'  # Replace with your actual file path
+file_path = './dataset.txt'
 parsed_data = parse_file(file_path)
 
 projects_data = parsed_data['projects']
@@ -17,3 +17,25 @@ print("Projects: ", projects_data)
 print("Precedence: ", precedence_data)
 print("Duration & Resources: ", duration_resources_data)
 print("Resource Availability: ", resource_availability_data)
+
+# Create a dictionary to store the project variables
+jobs = {}
+for project in projects_data:
+    print(f"Project: {project}")
+    for job, job_data in precedence_data.items():
+        print(f"\nJob: {job}")
+        print(f"Succesors: {job_data['successors']}")
+        print(f"Duration: {duration_resources_data[job]['duration']}")
+        print(f"Resources: {duration_resources_data[job]['resources']}")
+
+        jobs[job] = {
+            'successors': job_data['successors'],
+            'duration': duration_resources_data[job]['duration'],
+            'resources': duration_resources_data[job]['resources']
+        }
+
+print("Jobs: ", jobs)
+
+
+# Start the CSP problem
+problem = Problem()
