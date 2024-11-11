9906547070579d59562ea92f50ad90e3a14a994f
Zé Castro <a20351@alunos.ipca.pt>
2024-11-11 18:12:37 +0000

    commented comments and added explaining comments

--- csp.py
+++ csp.py
@@ -7,26 +7,27 @@ from parse_file import parse_file
 file_path = './dataset.txt'
 parsed_data = parse_file(file_path)
 
+# Extract the data from the parsed file
 projects_data = parsed_data['projects']
 precedence_data = parsed_data['precedence']
 duration_resources_data = parsed_data['duration_resources']
 resource_availability_data = parsed_data['resource_availability']
 
 # Print the data
-print("Projects: ", projects_data)
-print("Precedence: ", precedence_data)
-print("Duration & Resources: ", duration_resources_data)
-print("Resource Availability: ", resource_availability_data)
+# print("Projects: ", projects_data)
+# print("Precedence: ", precedence_data)
+# print("Duration & Resources: ", duration_resources_data)
+# print("Resource Availability: ", resource_availability_data)
 
 # Create a dictionary to store the project variables
 jobs = {}
 for project in projects_data:
-    print(f"Project: {project}")
+    # print(f"Project: {project}")
     for job, job_data in precedence_data.items():
-        print(f"\nJob: {job}")
-        print(f"Succesors: {job_data['successors']}")
-        print(f"Duration: {duration_resources_data[job]['duration']}")
-        print(f"Resources: {duration_resources_data[job]['resources']}")
+        # print(f"\nJob: {job}")
+        # print(f"Succesors: {job_data['successors']}")
+        # print(f"Duration: {duration_resources_data[job]['duration']}")
+        # print(f"Resources: {duration_resources_data[job]['resources']}")
 
         jobs[job] = {
             'successors': job_data['successors'],
